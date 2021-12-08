68e5a909412d42457813057e79d3b4abe1ac86ef
Jiwon <yesjiwon5304@gmail.com>
2021-12-08 15:12:58 +0900

    [프로그래머스] Level1_3 - 콜라츠 추측

--- L1_4.최대공약수와 최소공배수.py
+++ L1_4.최대공약수와 최소공배수.py
@@ -2,14 +2,15 @@
 
 def solution(n, m):
     result = []
-
+    
+    # 범위 : 1 ~ 입력 중 작은수 / n,m 모두 나눠지는 숫자중에서 가장 큰 수
     divisor = [a for a in range(1,min(n,m)+1) if n % a == 0 and m % a == 0]
     result.append(divisor[-1])
 
+    # 범위 : 입력 중 큰 수 ~ 두 수의 곱 / 두 개의 입력으로 나눠지는 수 중에서 가장 작은 수
     multiple = [b for b in range(max(n,m),(n*m)+1) if b % n == 0 and b % m == 0]
     result.append(multiple[0])
     
-    print(result)
     return result
 
 solution(8,3)
