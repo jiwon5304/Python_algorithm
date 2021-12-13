578ec35dc47f089dfaed662e9498e5ac2ca4da0f
Jiwon <yesjiwon5304@gmail.com>
2021-12-13 19:06:07 +0900

    [프로그래머스] Level1_6 - 제일 작은 수 제거하기 - 짝수와 홀수

---
+++ L1_9.짝수와 홀수.py
@@ -0,0 +1,8 @@
+def solution(num):
+    return "Even" if num % 2 == 0 else "Odd"
+    
+# def solution(num):    
+#     if num % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
